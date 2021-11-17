1d443f51334c622ada19b3af3228b86f8234bb34
tvannost <tvannost@hotmail.com>
2021-11-17 10:08:44 -0500

    added roles tabel

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Roles::factory(3)->createMany([
+            'title' => 'Admin',
+            'title' => 'Librarian',
+            'title' => 'Cardholder',
+        ]);
+    }
+}
